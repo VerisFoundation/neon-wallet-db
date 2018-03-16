f40eec1b052fa3ab7577ade314f1d603d2a358da
Jason L Perry <jason@suncoast.io>
2018-03-16 16:30:30 -0400

    Configuration

--- api/db.py
+++ api/db.py
@@ -3,21 +3,18 @@ import os
 import redis
 from rq import Queue
 
-MONGOUSER = os.environ.get('MONGOUSER')
-MONGOPASS = os.environ.get('MONGOPASS')
-MONGOURL = os.environ.get('MONGOURL')
-MONGOAPP = os.environ.get('MONGOAPP')
-MONGOURL = "mongodb://{}:{}@{}/{}".format(MONGOUSER, MONGOPASS, MONGOURL, MONGOAPP)
+MONGO_APP = 'neon-wallet-db'
+MONGO_URL = os.environ.get('MONGO_URL')
 
-client = MongoClient(MONGOURL)
-db = client[MONGOAPP]
+client = MongoClient(MONGO_URL)
+db = client[MONGO_APP]
 
 # db["meta"].insert_one({"name":"lastTrustedBlock", "value":1162327})
 # db["meta"].insert_one({"name":"lastTrustedTransaction", "value":1162327})
 
 # redis
 
-redis_url = os.environ.get('REDISTOGO_URL')
+redis_url = os.environ.get('REDIS_URL')
 
 redis_db = redis.from_url(redis_url)
 
